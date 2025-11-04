ec7e2dbd3f146d4e11615f0da8e7daae3301d1e5
feri-irawan <feriirawan.akun@gmail.com>
2025-11-04 10:05:01 +0800

    feat(app): add keyboard controller for enhanced input handling
    Integrates `react-native-keyboard-controller` by wrapping the root layout. This provides more control over keyboard appearance and animations, improving the user experience with text input fields.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -8,6 +8,7 @@ import { StatusBar } from 'expo-status-bar';
 import { useColorScheme } from 'nativewind';
 import { QueryProvider } from '@/common/contexts/Query';
 import { I18nInitializer } from '@/common/i18n';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,9 +22,11 @@ export default function RootLayout() {
     <I18nInitializer>
       <QueryProvider>
         <ThemeProvider value={NAV_THEME[colorScheme ?? 'light']}>
-          <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
-          <Stack />
-          <PortalHost />
+          <KeyboardProvider>
+            <StatusBar style={colorScheme === 'dark' ? 'light' : 'dark'} />
+            <Stack />
+            <PortalHost />
+          </KeyboardProvider>
         </ThemeProvider>
       </QueryProvider>
     </I18nInitializer>
